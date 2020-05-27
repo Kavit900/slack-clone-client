0f7dbe9adece48ae1d6354dfa970732af7db1551
Kavit Mehta <kavitmm@amazon.com>
2020-05-26 18:07:10 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import Routes from './routes';
+
+const App = (
+  <Routes />
+);
+
+ReactDOM.render( App,
   document.getElementById('root')
 );
 
